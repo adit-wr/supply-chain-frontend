b6f657ecb7e94ac89ee69088a3daa3ea476743fd
adit-wr <ganding0002nd@gmail.com>
2024-10-25 02:43:56 +0700

    add component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Navbar @navigate-to="handleNavigation" />
+    <div class="main-content">
+      <div v-if="currentPage === 'user'">
+        <UserComponent />
+      </div>
+      <div v-else-if="currentPage === 'item'">
+        <ItemComponent />
+      </div>
+      <div v-else-if="currentPage === 'transaction'">
+        <TransactionComponent />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import Navbar from "./components/Navbar.vue";
+  import UserComponent from "./components/user/UserList.vue";
+  import ItemComponent from "./components/item/ItemList.vue";
+  import TransactionComponent from "./components/transaction/TransactionList.vue";
+  export default {
+    components: {
+      Navbar,
+      UserComponent,
+      ItemComponent,
+      TransactionComponent,
+    },
 
-nav {
-  padding: 30px;
-}
+    data() {
+      return {
+        currentPage: "user",
+      };
+    },
+    methods: {
+      handleNavigation(page) {
+        this.currentPage = page;
+      },
+    },
+  };
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .main-content {
+    flex: 1;
+    padding: 20px;
+    background-color: #ffffff;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a href="#" class="navbar-item">adet</a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" @click.prevent="navigateTo('user')">User</a>
+        <a class="navbar-item" @click.prevent="navigateTo('item')">Item</a>
+        <a class="navbar-item" @click.prevent="navigateTo('transaction')">Transaction</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    methods: {
+      navigateTo(page) {
+        this.$emit('navigate-to', page)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .navbar {
+    background-color: #4b4b4b;
+    color: #fff;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    flex-wrap: wrap;
+  }
+
+  .navbar-brand {
+    display: flex;
+    align-items: center;
+  }
+
+  .navbar-item {
+    color: #fff;
+    padding: 10px 15px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  .navbar-item:hover {
+    background-color: #2b2b2b;
+    border-radius: 4px;
+  }
+
+  .navbar-menu {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+
+  @media (max-width: 768px) {
+    .navbar-menu{
+      width: 100px;
+      justify-content: center;
+    }
+
+    .navbar-start{
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      align-items: center;
+    }
+
+    .navbar-item {
+      width: 100%;
+      text-align: center;
+      padding: 15px 0;
+    }
+
+  }
+</style>
